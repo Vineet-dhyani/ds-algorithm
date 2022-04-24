0d14aadbc722223c911a6dad21e498016ad5b831
Vineet-dhyani <97015062+Vineet-dhyani@users.noreply.github.com>
2022-04-24 10:20:36 +0530

    hash table

---
+++ HashTable.java
@@ -0,0 +1,117 @@
+package upgrad.com;
+
+public class HashTable {
+
+	private HashNode[] buckets;
+	private int numOfBuckets;
+	private int size;
+	
+	
+	public HashTable(){
+		this(10);// default capacity
+	}
+	
+	public HashTable(int capacity) {
+		this.numOfBuckets=capacity;
+		buckets=new HashNode[numOfBuckets];
+		this.size=0;
+	}
+	
+	private class HashNode{
+		private Integer key;
+		private String value;
+		private HashNode next;
+		
+		public  HashNode(Integer key,String value) {
+			this.key=key;
+			this.value=value;
+			
+		}
+	}
+	public int size() {
+		return size;
+	}
+	public boolean isEmpty() {
+		return size==0;
+		
+	}
+	public void put(Integer key ,String value) {
+		if(key==null||value ==null) {
+			throw new IllegalArgumentException("key or value is null");
+		}
+		int bucketIndex=getBucketIndex(key);
+		HashNode head=buckets[bucketIndex];
+		while(head!=null) {
+			if(head.key.equals(key)) {
+				head.value=value;
+				return;
+			}
+			head.next=head;
+		}
+		size++;
+		head=buckets[bucketIndex];
+		HashNode node=new HashNode(key,value);
+		node.next=head;
+		buckets[bucketIndex]=node;
+		
+	}
+	public String get(Integer key) {
+		if(key==null) {
+			throw new IllegalArgumentException("key is null");
+		}
+		int bucketIndex=getBucketIndex(key);
+		HashNode head=buckets[bucketIndex];
+		while(head!=null) {
+			if(head.key.equals(key)) {
+				return head.value;
+			}
+			head=head.next;
+		}
+		return null;
+	}
+
+	public String remove(Integer key) {
+		if(key==null) {
+			throw new IllegalArgumentException("key is null");
+		}
+		int bucketIndex=getBucketIndex(key);
+		HashNode head=buckets[bucketIndex];
+		HashNode previous=null;
+		while(head!=null) {
+			if(head.key.equals(key)) {
+				break;
+			}
+			previous=head;
+			head=head.next;
+		}
+		if(head==null) {
+			return null;
+		}
+		size--;
+		if(previous!=null) {
+		previous.next=head.next;	
+		}else {
+			buckets[bucketIndex]=head.next;
+		}
+	return head.value;
+	
+}
+
+	
+	private int getBucketIndex(Integer key) {
+		return key%numOfBuckets;
+	}
+	
+
+public static void main(String args[]) {
+	HashTable table=new HashTable(10);
+	table.put(105,"tom");
+	table.put(21,"sana");
+	table.put(21,"marry");
+	System.out.println(table.size());
+	System.out.println(table.remove(21));
+	System.out.println(table.size());
+	System.out.println(table.remove(105));
+	System.out.println(table.size());
+	System.out.println(table.get(105));
+}}
